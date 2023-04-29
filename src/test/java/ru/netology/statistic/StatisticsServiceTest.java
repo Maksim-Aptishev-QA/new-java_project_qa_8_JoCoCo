3c3a8745df6e66909fdda1ea6089ceae4b1bace5
Maksim Aptishev GitHub <maximus.testqa777@gmail.com>
2023-04-29 19:45:50 +0300

    test successful

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 30};
+        long expected = 30;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
